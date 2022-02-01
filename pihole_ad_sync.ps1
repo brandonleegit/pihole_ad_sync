40d7e5e27f187f8587c29172397fdfe9a4df1555
unknown <22154488+brandonleegit@users.noreply.github.com>
2022-02-01 16:17:15 -0600

    Made changes to fix error with Windows Server 2022 DNS

--- pihole_ad_sync.ps1
+++ pihole_ad_sync.ps1
@@ -1,12 +1,12 @@
-#Brandon Lee - Virtualizationhowto.com - v1.0
+#Brandon Lee - Virtualizationhowto.com - v1.1
 #Set variables for the script to run
 $filename = "c:\custom.list"
 $domain = 'mydomain.local'
 $scppw = 'password'
 $piholeconnect = 'dnscopy@pihole01.cloud.local:/home/dnscopy/'
 
-#Run the DNS command to export entries to a file
-Get-DnsServerResourceRecord -ZoneName $domain | where-object {$_.HostName -like "*.$domain" -and $_.Hostname -notlike "_*"} | select @{n='IP';E={$_.recorddata.IPV4Address}}, hostname | ft -HideTableHeaders | Out-File $filename
+#Run the DNS command to export entries to a fi
+Get-DnsServerResourceRecord -ZoneName $domain | select-object @{n='IP';E={$_.recorddata.IPV4Address}}, {$_.hostname+".cloud.local"} | ft -HideTableHeaders | Out-File $filename
 
 #Trim blank lines from the resulting file
 (gc $filename) | ? {$_.trim() -ne "" } | set-content $filename
